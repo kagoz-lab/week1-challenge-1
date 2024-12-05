4f13932452cd06f4eb16b9825f503d3208951d12
kagoz-lab <kagokevin33@gmail.com>
2024-12-05 23:22:11 +0300

    second commit

--- challenge 1/index.js
+++ challenge 1/index.js
@@ -1,9 +1,9 @@
-const points = ("Enter the student's marks (0 - 100):");
+/*const points = prompt("Enter the student's marks (0 - 100):");
   
 const score = (points);
   
 //So here you have to give the range of the marks entered by the user not to exceed 100 and not to go below 0
-  
+  function scoreEvaluator(){
 if (score < 0 || score > 100 || (!score)) {//The second or is for Not a number.
   
     console.log("Invalid input. Please enter a number between 0 and 100.");//If the score is <0 or is >100,it notifies the user that the data entered is invalid and suggests that they enter valid data.
@@ -28,7 +28,44 @@ if (score < 0 || score > 100 || (!score)) {//The second or is for Not a number.
     console.log("Grade: E");//If the score is 39 and below, it displays the grade "E"
   
   }
-  
-  }
-  scoreEvaluator(50)
+}*/
+/*function studentGrade(Number){
+    
+  if (Number >= 0 && Number <= 100) {
+      if (Number > 79) {
+          return('Grade: A');
+      } else if (Number >= 60 && Number>= 78) {
+          return('Grade: B');
+      } else if (Number>= 50 && Number>= 59) {
+          return('Grade: C');
+      } else if (Number>= 40 &&Number >= 49) {
+          return('Grade: D');
+      } else if (Number<40 && Number>= 0)
+          return('Grade: E');
+      }
+      else {
+      console.log(`pls enter valid value`);
 
+      }
+    }
+console.log(studentGrade(45))*/
+
+function scoreGrade(score){
+  if (score >= 0 && score <= 100) {
+      if (score > 79) {
+          return 'Grade: A';
+      } else if (score >= 60 && score <= 79) {
+          return 'Grade: B';
+      } else if (score >= 50 && score <= 59) {
+          return 'Grade: C';
+      } else if (score >= 40 && score <= 49) {
+          return 'Grade: D';
+      } else {
+          return 'Grade: E';
+      }
+  } else {
+      console.log('Please enter a valid value.');
+  }
+}
+
+console.log(scoreGrade(45));  // Output: Grade: D
